750efb7f0650177b9f630592dfe1b30359519dfa
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-07-11 11:19:16 +0200

    Finished section 12 behind the scenes of react and optimization

--- attachments/12 Behind the Scenes/01-starting-project/src/components/Counter/Counter.jsx
+++ attachments/12 Behind the Scenes/01-starting-project/src/components/Counter/Counter.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react';
+import { useState, memo, useCallback, useMemo, useEffect } from "react";
 
-import IconButton from '../UI/IconButton.jsx';
-import MinusIcon from '../UI/Icons/MinusIcon.jsx';
-import PlusIcon from '../UI/Icons/PlusIcon.jsx';
-import CounterOutput from './CounterOutput.jsx';
-import { log } from '../../log.js';
+import IconButton from "../UI/IconButton.jsx";
+import MinusIcon from "../UI/Icons/MinusIcon.jsx";
+import PlusIcon from "../UI/Icons/PlusIcon.jsx";
+import CounterOutput from "./CounterOutput.jsx";
+import { log } from "../../log.js";
+import CounterHistory from "./CounterHistory.jsx";
 
 function isPrime(number) {
-  log(
-    'Calculating if is prime number',
-    2,
-    'other'
-  );
+  log("Calculating if is prime number", 2, "other");
   if (number <= 1) {
     return false;
   }
@@ -27,35 +24,63 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
-  log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+const Counter = memo(function Counter({ initialCount }) {
+  // memo only prevents funcion executions that are triggered by the parent component, it checks if any values (props or state has changed), memo isn't needed here anymore but we leave it here as an example
+  log("<Counter /> rendered", 1);
+  const initialCountIsPrime = useMemo(
+    // this causes the method isPrime to only be re-executed if the initial count changes, if it didn't have a dependency the method would only be executed once.
+    () => isPrime(initialCount),
+    [initialCount]
+  );
 
-  const [counter, setCounter] = useState(initialCount);
+  // useEffect(() => { 
+  //   setCounterChanges([{ value: initialCount, id: Math.random() * 100 }]); // here we reset the array
+  // }, [initialCount]);
 
-  function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
-  }
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([
+    { value: initialCount, id: Math.random() * 100 },
+  ]);
 
-  function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
-  }
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange.value,
+    0
+  );
+
+  const handleDecrement = useCallback(function handleDecrement() {
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [
+      { value: -1, id: Math.random() * 100 },
+      ...prevCounterChanges,
+    ]);
+  }, []);
+
+  const handleIncrement = useCallback(function handleIncrement() {
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [
+      { value: 1, id: Math.random() * 100 },
+      ...prevCounterChanges,
+    ]);
+  }, []);
 
   return (
     <section className="counter">
       <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{' '}
-        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+        The initial counter value was <strong>{initialCount}</strong>. It{" "}
+        <strong>is {initialCountIsPrime ? "a" : "not a"}</strong> prime number.
       </p>
       <p>
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        <CounterOutput value={currentCounter} /> {/* counter */}
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
       </p>
+      <CounterHistory history={counterChanges} />
     </section>
   );
-}
+});
+
+export default Counter;
