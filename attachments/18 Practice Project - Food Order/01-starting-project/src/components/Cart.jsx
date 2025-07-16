8d0584de413d3d8e6368c688d6b8ac155e2fc164
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-07-16 13:49:19 +0200

    Added the CartItem component and use it in the Cart component

--- attachments/18 Practice Project - Food Order/01-starting-project/src/components/Cart.jsx
+++ attachments/18 Practice Project - Food Order/01-starting-project/src/components/Cart.jsx
@@ -4,6 +4,7 @@ import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 export default function Cart() {
   const cartContext = useContext(CartContext);
@@ -22,9 +23,14 @@ export default function Cart() {
       <h2>Your cart</h2>
       <ul>
         {cartContext.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            id={item.id}
+            name={item.name}
+            price={item.price}
+            quantity={item.quantity}
+            onIncrease={() => cartContext.addItem(item)}
+            onDecrease={() => cartContext.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
