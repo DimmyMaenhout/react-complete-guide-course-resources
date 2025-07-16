b0f8cdce735381827700984e9419743bd52f45f7
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-07-16 10:57:05 +0200

    Updated the MealItem component after watching the solution video

--- attachments/18 Practice Project - Food Order/01-starting-project/src/components/MealItem.jsx
+++ attachments/18 Practice Project - Food Order/01-starting-project/src/components/MealItem.jsx
@@ -1,19 +1,19 @@
 export default function MealItem({ meal }) {
   return (
-    <div className="meal-item">
+    <li className="meal-item">
       <article>
-        {/* <div> */}
-        <img src={`http://localhost:3000/${meal.image}`} alt="food image" />
-        {/* </div> */}
-        <div className="center meal-item-actions">
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <div>
           <h3>{meal.name}</h3>
 
           <p className="meal-item-price">${meal.price}</p>
 
           <p className="meal-item-description">{meal.description}</p>
-          <button className="button">Add to Cart</button>
         </div>
+        <p className="meal-item-actions">
+          <button className="button">Add to Cart</button>
+        </p>
       </article>
-    </div>
+    </li>
   );
 }
