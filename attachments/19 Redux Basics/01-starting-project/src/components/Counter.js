1a57020ee6d28016716b7a349e2fad1d68316d75
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-07-17 14:23:28 +0200

    Used Redux without Redux Toolkit

--- attachments/19 Redux Basics/01-starting-project/src/components/Counter.js
+++ attachments/19 Redux Basics/01-starting-project/src/components/Counter.js
@@ -1,26 +1,35 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, connect } from "react-redux";
+import { Component } from "react";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch(); // this gives us back a dispatch function which we can execute against our Redux store, in this case "increment" & "decrement"
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
   };
 
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
@@ -29,3 +38,51 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// Example with class component
+
+// class Counter extends Component {
+
+//   incrementHandler() {
+//     this.props.incrememt();
+//   }
+
+//   decrementHandler() {
+//     this.props.decrememt();
+//   }
+
+//   toggleCounterHandler() {
+
+//   }
+
+//   render() {
+//     return <main className={classes.counter}>
+//       <h1>Redux Counter</h1>
+//       <div className={classes.value}>{this.props.counter}</div>
+//       <div>
+//         <button onClick={this.incrementHandler.bind(this)}>Increment</button> {/* this in the bind function refers to the class */}
+//         <button onClick={this.decrementHandler.bind(this)}>decrement</button>
+//       </div>
+//       <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//     </main>;
+//   }
+// }
+
+// const mapStateToProps = state => {
+//   return {
+//     counter: state.counter
+//   };
+// }
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     incrememt: () => dispatch({ type: "increment" }),
+//     decrememt: () => dispatch({ type: "decrement" })
+//   }
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter); // This can look strange but "connect" is a so-called higher order component. We execute the connect function, it then returns a new funcion and we execute this returned function as well and to this returned function we pass counter
+// Why do we do it like this? Because to connect, when we execute this, we also pass something. Connect also wants some arguments, 2 arguments that are functions. The first function is a function that maps a Redux state to props which will be received in this component then.
+// Hence we call this function mapStateToProps. This is a function which receives the Redux state and then this returns an object where the keys will be available as props in the receiving component (in this case the Counter component)
+// The second argument is antoher function which is typically called mapDispatchToProps now the idea is to store dispatch functions to props
+// When using  "connect", react Redux will still setup a subscription and manage a subscription for you, it's an alternative to the hooks "useDispatch" and "useSelector"
--- attachments/19 Redux Basics/01-starting-project/src/store/index.js
+++ attachments/19 Redux Basics/01-starting-project/src/store/index.js
@@ -1,12 +1,30 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
+  // We must always set all the other states when we update a piece of state
   if (action.type === "increment") {
-    return { counter: state.counter + 1 };
+    // the existing state must never be changed, instead it should always be overwritted by returning a new state object, this is because objects & arrays are reference values in JS it's easy to accidently override and change the existing state
+    return { counter: state.counter + 1, showCounter: state.showCounter };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
   }
 
   if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
+    return { counter: state.counter - 1, showCounter: state.showCounter };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
   }
 
   return state;
