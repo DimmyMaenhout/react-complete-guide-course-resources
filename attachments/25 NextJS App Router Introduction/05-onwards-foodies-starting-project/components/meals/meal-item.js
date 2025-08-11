5a7e750cff0627b544010b5efbd4892e10c4560c
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-08-11 11:44:22 +0200

    Added the components MealsGrid & MealItem and added a sqlite3 database to store the meals

---
+++ attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/meals/meal-item.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import classes from "./meal-item.module.css";
+
+export default function MealItem({ title, slug, image, summary, creator }) {
+  return (
+    <article className={classes.meal}>
+      <header>
+        <div className={classes.image}>
+          <Image src={image} alt={title} fill />
+          {/* fill tells NextJS that the available space should be filled with that image as defined by its parent components*/}
+        </div>
+        <div className={classes.headerText}>
+          <h2>{title}</h2>
+          <p>by {creator}</p>
+        </div>
+      </header>
+      <div className={classes.content}>
+        <p className={classes.summary}>{summary}</p>
+        <div className={classes.actions}>
+          <Link href={`/meals/${slug}`}>View Details</Link>
+        </div>
+      </div>
+    </article>
+  );
+}
