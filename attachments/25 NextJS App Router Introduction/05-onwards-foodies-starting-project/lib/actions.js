cae42ab13d1fa9081f15c4af7bde52e974b57a02
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-08-12 13:37:53 +0200

    Invalidate the cache (for the /meals page) when adding a new meal, otherwise the production build would use the cached data and we wouldn't see the newly added meals

--- attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/lib/actions.js
+++ attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/lib/actions.js
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
 // because of the "use server" at the top of the file all actions in this file will be treated as Server Actions.
 
@@ -34,10 +35,14 @@ export async function shareMeal(prevState, formData) {
     // instead of throwing everything away with an error, we can handle it in a more elegant way. We can also return values, return response objects to be precise, it should be a serializable method so it shouldn't have any method becasue they would be lost when being sent to the client
 
     return {
-        message: "Invalid input."
-    }
+      message: "Invalid input.",
+    };
   }
 
   await saveMeal(meal);
+
+  // when we add a new meal we need to tell NextJS to throw away it's cahce or part of it
+  revalidatePath("/meals"); // this function tells NextJS to revalidate the cache that belongs to a certain route part, by default there is also a second parameter in this function which is set to "page", if we replace this with "layout" then the nested pages would also be revalidated.
+  // Invalidating the cache of the entire website is also possible by doing this revalidatePath("/", "layout")
   redirect("/meals");
 }
