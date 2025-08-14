881b52f6222f6dfdaf1d9b8f2b7c6a5ea59ac609
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-08-14 19:47:58 +0200

    Added compound components (meerdere componenten gebouwd die werken als 1 geheel, denk aan <select> & <option>)

--- attachments/27 Patterns/01-starting-project/src/App.jsx
+++ attachments/27 Patterns/01-starting-project/src/App.jsx
@@ -1,5 +1,43 @@
+import Accordion from "./components/Accordion/Accordion";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why works with us?</h2>
+        <Accordion className="accordion">
+          <Accordion.Item id="experience" className="accordion-item">
+            <Accordion.Title className="accordion-item-title">
+              We got 20 years of experience
+            </Accordion.Title>
+            <Accordion.Content className="accordion-item-content">
+              <article>
+                <p>You can&apos;t go wrong with us.</p>
+                <p>
+                  We are in the business of planning highly individualized
+                  vacation trips for more than 20 years
+                </p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+          <Accordion.Item className="accordion-item" id="local-guides">
+            <Accordion.Title className="accordion-item-title">
+              We're working with local guides
+            </Accordion.Title>
+            <Accordion.Content className="accordion-item-content">
+              <article>
+                <p>We are not doig this alone from our office.</p>
+                <p>
+                  Instead. we are working with local guides to ensure a safe and
+                  pleasant vacation.
+                </p>
+              </article>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
