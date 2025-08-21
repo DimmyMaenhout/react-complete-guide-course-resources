38af47b938dacde79e8681f155ce5ed3ba3c5f7f
Dimmy Maenhout <dimmy.maenhout@telenet.be>
2025-08-21 11:26:21 +0200

    Added and unzipped the starter project for bonus section 37 + completed the practice project

---
+++ attachments/37 Bonus Time to Practice Component basics/01-starting-setup/src/components/KeyConceptItem.js
@@ -0,0 +1,9 @@
+export default function KeyConceptItem(props) {
+  return (
+    <li className="concept">
+      <img src={props.item.image} alt={props.item.title} />
+      <h2>{props.item.title}</h2>
+      <p>{props.item.description}</p>
+    </li>
+  );
+}
